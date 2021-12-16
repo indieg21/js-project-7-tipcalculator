4813be50d5ed3b524a03fa4ce168195058aff7b4
indieg21 <Indiagraham21@gmail.com>
2021-12-16 09:20:34 -0500

    update var to const

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var total = document.getElementById("totalBill");
+//if value has to change then we use let and the value is not changing then we can use const. 
+
+const total = document.getElementById("totalBill");
 total.style.color = "red";
-var bill = document.getElementById("billInput");
-var tip = document.getElementById("tipInput");
-var btn = document.getElementById("btn");
+const bill = document.getElementById("billInput");
+const tip = document.getElementById("tipInput");
+const btn = document.getElementById("btn");
 btn.addEventListener("click", tipCalculator);
 
 function tipCalculator() {
-  let billAmount = parseFloat(bill.value);
-  let tipPerecentage = parseFloat(tip.value);
-  let tipAmount = (tipPerecentage / 100) * billAmount;
-  let totalAmount = billAmount + tipAmount;
+  const billAmount = parseFloat(bill.value);
+  const tipPerecentage = parseFloat(tip.value);
+  const tipAmount = (tipPerecentage / 100) * billAmount;
+  const totalAmount = billAmount + tipAmount;
   //console.log(billAmount, tipPerecentage, tipAmount, totalAmount)
   total.innerHTML = `
           <h3>your total bill amount including tip: ${totalAmount}</h3>
@@ -17,3 +19,4 @@ function tipCalculator() {
 
           `;
 }
+
